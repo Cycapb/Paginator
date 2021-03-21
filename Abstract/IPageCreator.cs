1cdaac9bd54695f6551f28effd1786f70b04b34a
Cycapb <aka.apostol@gmail.com>
2021-03-21 21:00:12 +0500

    Change parameter name of method IPageCreator.CreatePages from pageUrl to urlCreator

--- Abstract/IPageCreator.cs
+++ Abstract/IPageCreator.cs
@@ -5,6 +5,6 @@ namespace Paginator.Abstract
 {
     public interface IPageCreator
     {
-        IHtmlContent CreatePages(int itemsPerPage, int currentPage, int totalItems, string idToReplace, Func<int, string> pageUrl);
+        IHtmlContent CreatePages(int itemsPerPage, int currentPage, int totalItems, string idToReplace, Func<int, string> urlCreator);
     }
 }
